5266680cc133e25194bdf5f7af19646b06e127b5
river <kimss.river@gamil.com>
2020-09-16 15:21:27 +0900

    JS : add APi

--- weather.js
+++ weather.js
@@ -1,4 +1,21 @@
+const weather = document.querySelector('.js-weather');
+
+const API_KEY = '';
 const COORDS = 'coords';
+
+function getWeather(lat, lng) {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+  )
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (json) {
+      const temperature = json.main.temp;
+      const place = json.name;
+      weather.innerText = `${temperature} @ ${place}`;
+    });
+}
 
 function saveCoords(coordsObj) {
   localStorage.setItem(COORDS, JSON.stringify(coordsObj));
@@ -12,6 +29,7 @@ function handleGeoSucces(position) {
     longitude,
   };
   saveCoords(coordsObj);
+  getWeather(latitude, longitude);
 }
 
 function handleGeoError() {
@@ -27,6 +45,8 @@ function loadCoords() {
   if (loadedCoords === null) {
     askForCoords();
   } else {
+    const parsCoords = JSON.parse(loadedCoords);
+    getWeather(parsCoords.latitude, parsCoords.longitude);
   }
 }
 
